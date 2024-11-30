6019902ac8315cf2227954439e982aaec6e2c4b5
HIMDanny <danilbliz0201@gmail.com>
2024-11-30 22:55:42 +0200

    feat: enhance Input component with variant support for size

--- client/src/components/ui/input.tsx
+++ client/src/components/ui/input.tsx
@@ -1,16 +1,29 @@
+import type { VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
+import { cva } from 'class-variance-authority';
 
 import * as React from 'react';
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
-  ({ className, type, ...props }, ref) => {
+const inputVariants = cva('flex w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm', {
+  variants: {
+    size: {
+      sm: 'h-9',
+      default: 'h-10',
+    },
+  },
+  defaultVariants: {
+    size: 'default',
+  },
+});
+
+export interface InputProps extends Omit<React.ComponentPropsWithoutRef<'input'>, 'size'>, VariantProps<typeof inputVariants> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, size, ...props }, ref) => {
     return (
       <input
         type={type}
-        className={cn(
-          'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-          className,
-        )}
+        className={cn(inputVariants({ className, size }))}
         ref={ref}
         {...props}
       />
