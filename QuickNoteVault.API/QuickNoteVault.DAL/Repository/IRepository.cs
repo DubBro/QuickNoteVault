42b1acb8c9c8a2ff15101ddcc3a42c671a4e3a05
Hlib Semeniuk <6918493@stud.nau.edu.ua>
2024-12-15 21:25:17 +0200

    QNV-39 Implemented generic Repository pattern with IRepository and Repository classes

---
+++ QuickNoteVault.API/QuickNoteVault.DAL/Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace QuickNoteVault.DAL.Repository
+{
+    public interface IRepository<T>
+        where T : class
+    {
+        Task<ICollection<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        Task DeleteByIdAsync(int id);
+        Task<ICollection<T>> FindByConditionAsync(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ QuickNoteVault.API/QuickNoteVault.DAL/Repository/Repository.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace QuickNoteVault.DAL.Repository
+{
+    public class Repository<T> : IRepository<T>
+        where T : class
+    {
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(DbContext context)
+        {
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            var entityToDelete = await _dbSet.FindAsync(id);
+
+            if (entityToDelete != null)
+            {
+                _dbSet.Remove(entityToDelete);
+            }
+        }
+
+        public async Task<ICollection<T>> FindByConditionAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
+
+        public async Task<ICollection<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity == null)
+            {
+                throw new KeyNotFoundException($"Entity with id {id} not found.");
+            }
+
+            return entity;
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+    }
+}
