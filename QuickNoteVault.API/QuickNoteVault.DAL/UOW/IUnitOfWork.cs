89ab36fa54266ad44fc662ddc5fad654e30ed7c1
Hlib Semeniuk <6918493@stud.nau.edu.ua>
2024-12-15 22:10:41 +0200

    QNV-38 Implemented generic UnitOfWork pattern

---
+++ QuickNoteVault.API/QuickNoteVault.DAL/UOW/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using QuickNoteVault.DAL.Repository;
+
+namespace QuickNoteVault.DAL.UOW
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task SaveAsync();
+        IRepository<T> GetRepository<T>()
+            where T : class;
+    }
+}
---
+++ QuickNoteVault.API/QuickNoteVault.DAL/UOW/UnitOfWork.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickNoteVault.DAL.Repository;
+
+namespace QuickNoteVault.DAL.UOW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _context;
+        private readonly Dictionary<Type, object> _repositories = new Dictionary<Type, object>();
+        private bool _disposed = false;
+
+        public UnitOfWork(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IRepository<T> GetRepository<T>()
+            where T : class
+        {
+            var type = typeof(T);
+            if (!_repositories.ContainsKey(type))
+            {
+                var repositoryType = typeof(Repository<>).MakeGenericType(type);
+                var repositoryInstance = Activator.CreateInstance(repositoryType, _context);
+
+                if (repositoryInstance != null)
+                {
+                    _repositories[type] = repositoryInstance;
+                }
+                else
+                {
+                    throw new InvalidOperationException($"Failed to create repository for type {type.FullName}");
+                }
+            }
+
+            return (IRepository<T>)_repositories[type];
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed && disposing)
+            {
+                _context.Dispose();
+            }
+
+            _disposed = true;
+        }
+    }
+}
