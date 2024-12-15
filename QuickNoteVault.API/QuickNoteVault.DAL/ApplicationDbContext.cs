8518e05ac4fcc3847f3d32d8a1d97ad59705d32e
Hlib Semeniuk <6918493@stud.nau.edu.ua>
2024-12-15 19:15:38 +0200

    QNV-35 Added ApplicationDbContext

---
+++ QuickNoteVault.API/QuickNoteVault.DAL/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickNoteVault.DAL.Entities;
+
+namespace QuickNoteVault.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        required public DbSet<NoteEntity> Notes { get; set; }
+        required public DbSet<UserEntity> Users { get; set; }
+    }
+}
