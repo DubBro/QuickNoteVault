28e8fad93fbeb236aab0b8a981863e9793fd670b
Hlib Semeniuk <6918493@stud.nau.edu.ua>
2024-12-21 12:37:33 +0200

    QNV-98 QNV-99 QNV-100 feat: Implemented GetAll, Update, and Delete methods in NoteController

--- QuickNoteVault.API/QuickNoteVault.API/Controllers/NoteController.cs
+++ QuickNoteVault.API/QuickNoteVault.API/Controllers/NoteController.cs
@@ -51,8 +51,24 @@ public class NoteController : ControllerBase
     [HttpGet]
     public async Task<IActionResult> GetAll(int userId)
     {
-        await Task.Delay(1000);
-        throw new NotImplementedException();
+        try
+        {
+            var noteModelList = await _noteService.GetAllByUserIdAsync(userId);
+            return Ok(_mapper.Map<List<NoteDTO>>(noteModelList));
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"NoteController - {ex.Message}");
+
+            if (ex is NoteNotFoundException)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return StatusCode(500);
+            }
+        }
     }
 
     [HttpPost]
@@ -82,14 +98,47 @@ public class NoteController : ControllerBase
     [HttpPut]
     public async Task<IActionResult> Update(NoteDTO noteDTO)
     {
-        await Task.Delay(1000);
-        throw new NotImplementedException();
+        try
+        {
+            var noteModel = _mapper.Map<NoteModel>(noteDTO);
+            await _noteService.UpdateAsync(noteModel);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"NoteController - {ex.Message}");
+
+            if (ex is NoteNotFoundException || ex is UserNotFoundException)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return StatusCode(500);
+            }
+        }
     }
 
     [HttpDelete]
     public async Task<IActionResult> Delete(int id)
     {
-        await Task.Delay(1000);
-        throw new NotImplementedException();
+        try
+        {
+            await _noteService.DeleteByIdAsync(id);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"NoteController - {ex.Message}");
+
+            if (ex is NoteNotFoundException)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return StatusCode(500);
+            }
+        }
     }
 }
