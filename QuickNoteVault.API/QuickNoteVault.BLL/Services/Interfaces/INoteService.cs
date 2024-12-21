3af7f475c23889ddc06bb5e248c5e3524512d0bd
DubBro <taras.melnyk.aka.krimyllex@gmail.com>
2024-12-21 07:36:15 +0200

    QNV-52 QNV-53 QNV-59 QNV-60 QNV-92 Created NoteService

---
+++ QuickNoteVault.API/QuickNoteVault.BLL/Services/Interfaces/INoteService.cs
@@ -0,0 +1,12 @@
+﻿using QuickNoteVault.BLL.Models;
+
+namespace QuickNoteVault.BLL.Services.Interfaces;
+
+public interface INoteService
+{
+    Task<ICollection<NoteModel>> GetAllByUserIdAsync(int userId);
+    Task<NoteModel> GetByIdAsync(int id);
+    Task AddAsync(NoteModel noteModel);
+    Task UpdateAsync(NoteModel noteModel);
+    Task DeleteByIdAsync(int id);
+}
